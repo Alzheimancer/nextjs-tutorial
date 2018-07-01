d3f23e0139e8d7fb7c222dfb7e886ace162bfe88
Alzheimancer <morleng2@gmail.com>
2018-07-01 12:49:14 +0700

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import MyLayout from "../components/MyLayout";
+
+const About = () => (
+  <MyLayout>
+    <div>
+      <h1>About BitzPrice</h1>
+      <p>Application to view Bitcoin prices</p>
+    </div>
+  </MyLayout>
+);
+
+export default About;
